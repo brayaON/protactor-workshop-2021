8cc2ee48dd0eb74d9834a123e1dc3cb95c5aa6a4
brayaON <bortizf@unal.edu.co>
2021-01-21 13:37:28 -0500

    Deactivate Selenium server

--- test/google.spec.ts
+++ test/google.spec.ts
@@ -2,11 +2,11 @@ import { browser } from 'protractor';
 
 describe('Given a SDET learning protractor', () => {
   describe('when open Google Page', () => {
-    beforeEach(() => {
-      browser.get('https://www.google.com');
+    beforeEach(async () => {
+      await browser.get('https://www.google.com');
     });
-    it('then should have a title', () => {
-      expect(browser.getTitle()).toEqual('Google')
+    it('then should have a title', async () => {
+      await expect(browser.getTitle()).toEqual('Google')
     });
   });
 });
