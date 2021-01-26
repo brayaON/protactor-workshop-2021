39f1af3793566ca08419d9c475f85d98fe2350e5
brayaON <bortizf@unal.edu.co>
2021-01-26 10:45:57 -0500

    Adding static code analyzer

--- test/google.spec.ts
+++ test/google.spec.ts
@@ -6,7 +6,7 @@ describe('Given a SDET learning protractor', () => {
       await browser.get('https://www.google.com');
     });
     it('then should have a title', async () => {
-      await expect(browser.getTitle()).toEqual('Google')
+      await expect(browser.getTitle()).toEqual('Google');
     });
   });
 });
