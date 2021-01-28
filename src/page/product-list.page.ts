b14894ace6980c60e01ffca20630aed63611062d
brayaON <bortizf@unal.edu.co>
2021-01-28 12:16:32 -0500

    Elements, filters and elements inside elements

--- src/page/product-list.page.ts
+++ src/page/product-list.page.ts
@@ -1,10 +1,12 @@
-import { $, browser, ElementFinder, ExpectedConditions } from 'protractor';
+import { $, $$, browser, ElementFinder, ExpectedConditions } from 'protractor';
 
 export class ProductListPage {
   private productAdded: ElementFinder;
+  private products;
 
   constructor() {
     this.productAdded = $('#center_column a.button.ajax_add_to_cart_button.btn.btn-default');
+    this.products = $('#block_top_menu li');
   }
 
   public async goToProductAddedModal() : Promise<void> {
@@ -13,5 +15,13 @@ export class ProductListPage {
 
   public async waitGoToProductAddedModal() : Promise<void> {
     return await browser.wait(ExpectedConditions.elementToBeClickable(this.productAdded), 4000);
+  }
+
+  private findByProduct(productName) {
+    $$('#block_top_menu li').filter(async (elem) => {
+      return elem.getText().then((text) => {
+        return text === productName;
+      });
+    }).first().click();
   }
 }
